5ded8bf8f4f7ea68cdc64866149ac92cab8d3ccd
chrislee973 <christopherlee973@gmail.com>
2021-11-03 14:00:08 -0700

    more plotz

--- covid.py
+++ covid.py
@@ -7,6 +7,7 @@ import pytz
 import streamlit.components.v1 as components
 import streamlit as st
 import plotly.graph_objects as go
+
 st.set_page_config(layout="wide")
 
 
@@ -15,113 +16,146 @@ from utils import *
 
 # READ MOST RECENT DATA
 # Yesterday's date is used to read in new data and send it to cache, since the most recent date with data available is the day before the present day
-# yesterday = date.today() - timedelta(1)
-tz = pytz.timezone('US/Pacific')
+tz = pytz.timezone("US/Pacific")
 yesterday = datetime.now(tz).date() - timedelta(1)
+
 
 @st.cache
 def read_data(yesterday):
-    df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv', 
-                        parse_dates=['date'], 
-                        )
-    df['date'] = df['date'].dt.date
-    drop_idx = df[df['location'].isin(df['continent'].unique()) 
-                | (df['location'] =='World') | (df['location'] =='European Union')].index
-    df= df.drop(drop_idx)
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",
+        parse_dates=["date"],
+    )
+    df["date"] = df["date"].dt.date
+    drop_idx = df[
+        df["location"].isin(df["continent"].unique())
+        | (df["location"] == "World")
+        | (df["location"] == "European Union")
+    ].index
+    df = df.drop(drop_idx)
+    df = df.dropna(subset=["continent"])
     return df
-df = read_data(yesterday)
 
+
+df = read_data(yesterday)
 
 # HEADING
 st.title("Covid-19 Global Cases")
+st.subheader(
+    """A global dashboard of daily new cases and other related metrics using data sourced from Our World in Data."""
+)
 st.write("By Christopher Lee")
-st.subheader("""A global dashboard of daily new cases and other related metrics using data sourced from Our World in Data.""")
-st.write("Use the side-panel on the left to select the metric you'd like to see as well as the date you'd like to view data for (the date with the latest available data is selected by default).")
 """***"""
 
+feature_map = {
+    "New cases": "new_cases",
+    "New cases per million people": "new_cases_per_million",
+    "New cases (7 day rolling average)": "new_cases_smoothed",
+    "New cases per million people (7 day rolling average)": "new_cases_smoothed_per_million",
+    "Total cases": "total_cases",
+    "Total cases per million people": "total_cases_per_million",
+}
 
-# SIDEBAR 
-# selected_feature = st.sidebar.selectbox('What metric would you like to view data for?', 
-#                                     ['New cases','New cases per capita', 'New cases (7 day rolling average)', 'New cases per capita (7 day rolling average)'])
-
-feature_map = {'New cases': 'new_cases', 
-                'New cases per capita': 'new_cases_per_million', 
-                'New cases (7 day rolling average)': 'new_cases_smoothed', 
-                'New cases per capita (7 day rolling average)': 'new_cases_smoothed_per_million', 
-                'Total cases': 'total_cases', 
-                'Total cases per capita': 'total_cases_per_million'}
-
-# most_recent = sorted(set(df.date), reverse=True)[0]#.strftime("%B %d, %Y")
-most_recent=yesterday
+most_recent = yesterday
 day_before_most_recent = yesterday - timedelta(1)
 first_date = sorted(set(df.date))[0]
 
 selected_feature_COLUMN, date_input_COLUMN = st.columns(2)
 with selected_feature_COLUMN:
-    selected_feature = st.selectbox('Metric', 
-                                    ['New cases','New cases per capita', 'New cases (7 day rolling average)', 'New cases per capita (7 day rolling average)'])
-# date_input = st.sidebar.date_input('Choose date to display data', most_recent, min_value=first_date, max_value=most_recent)
+    selected_feature = st.selectbox(
+        "Metric",
+        [
+            "New cases",
+            "New cases per million people",
+            "New cases (7 day rolling average)",
+            "New cases per million people (7 day rolling average)",
+        ],
+    )
 with date_input_COLUMN:
-    date_input = st.date_input(f'Choose date to display data(most recent date is {most_recent.strftime("%m/%d/%Y")})', most_recent, min_value=first_date, max_value=most_recent)
+    date_input = st.date_input(
+        f"Choose date to display data",
+        most_recent,
+        min_value=first_date,
+        max_value=most_recent,
+    )
 
-col1, col2 = st.columns([8,1])
 
 # CHOROPLETH
-fig = plotly_choropleth(df, date_input, selected_feature=feature_map.get(selected_feature, ''), 
-                        )
-with col1:
-    st.plotly_chart(fig, use_container_width=True)    
+fig = plotly_choropleth(
+    df,
+    date_input,
+    selected_feature=feature_map.get(selected_feature, ""),
+)
+st.plotly_chart(fig, use_container_width=True)
 
 st.write("""***""")
 
 # DAILY INCREASES CHART
-st.header(f"Countries with highest daily increase in {selected_feature.lower()}")
+st.subheader(
+    f"Countries with highest daily increase in {MAP_TITLE.get(feature_map.get(selected_feature, ''), '').lower()}"
+)
+st.write(
+    f"""From {day_before_most_recent.strftime("%B %d, %Y")} - {most_recent.strftime("%B %d, %Y")}"""
+)
 df_pivot = pivot(df, selected_feature, feature_map)
-percent_increase, absolute_increase = daily_increase(df_pivot, most_recent, day_before_most_recent)
-# with col2:
-#     col2.subheader('Top 20 countries with highest daily increase in '+ selected_feature.lower())
-#     option = st.selectbox("select option", ["% increase", "absolute increase"])
-#     if option == "% increase":
-#         # st.table(percent_increase)
-#         st.bar_chart(percent_increase, use_container_width=False)
-#     elif option == "absolute increase":
-#         st.table(absolute_increase)
-#     st.bar_chart(absolute_increase, use_container_width=False)
-chart1, chart2 = st.columns(2)
-with chart1:
-    percent_chart = plotly_bargraph(percent_increase, most_recent, day_before_most_recent, selected_feature = selected_feature, option = "% increase")
+percent_increase, absolute_increase = daily_increase(
+    df_pivot, most_recent, day_before_most_recent
+)
+
+bar1, bar2 = st.columns(2)
+with bar1:
+    percent_chart = plotly_bargraph(
+        percent_increase, most_recent, option="Percent increase"
+    )
     st.plotly_chart(percent_chart)
-with chart2:
-    abs_chart = plotly_bargraph(absolute_increase, most_recent, day_before_most_recent, selected_feature = selected_feature, option = "abs. increase")
+with bar2:
+    abs_chart = plotly_bargraph(
+        absolute_increase, most_recent, option="Absolute increase"
+    )
     st.plotly_chart(abs_chart)
 
-# st.write("""***""")
-
-# st.header('Top 20 countries with highest daily increase in '+ selected_feature.lower())
-# st.write(f'From {day_before_most_recent.strftime("%B %d, %Y")} - {most_recent.strftime("%B %d, %Y")}')
-# st.write('Ranked in decreasing order.')
-
-# st.write("""***""")
-
-# percent_increase_COLUMN, abs_increase_COLUMN = st.columns(2)
-
-# percent_increase_COLUMN.write('% increase')
-# percent_increase_COLUMN.table(percent_increase)
-
-# abs_increase_COLUMN.write('Absolute increase')
-# abs_increase_COLUMN.table(absolute_increase)
-
 st.write("""***""")
-# HIGHEST TOTAL CASES TABLE
-st.header("Countries with the highest total cases")
-st.write('As of ' + most_recent.strftime("%B %d, %Y"))
-per_capita = st.checkbox('View per-capita')
-if per_capita:
-    feature_name = 'Total cases per capita'
-else:
-    feature_name = 'Total cases'
 
+# HIGHEST TOTAL CASES TABLE
+st.subheader("Total cases")
+st.write("As of " + most_recent.strftime("%B %d, %Y"))
+per_capita = st.checkbox("View per-capita")
+if per_capita:
+    feature_name = "Total cases per million people"
+else:
+    feature_name = "Total cases"
+
+
+# st.table(most_cases.style.format("{:,}"))
+
+# total_cases_bar, total_cases_choropleth = st.columns(2)
+# with total_cases_choropleth:
+total_cases_fig = plotly_choropleth(df, most_recent, feature_map.get(feature_name, ""))
+st.plotly_chart(total_cases_fig, use_container_width=True)
+
+
+# with total_cases_bar:
 total_cases = pivot(df, feature_name, feature_map)
-most_cases = pd.DataFrame(total_cases[most_recent].sort_values(ascending=False)).head(20)
-most_cases.rename(columns = {most_cases.columns[0]: feature_name}, inplace=True)
-st.table(most_cases.style.format("{:,}"))
+most_cases = pd.DataFrame(total_cases[most_recent].sort_values(ascending=False)).head(
+    20
+)
+most_cases.rename(columns={most_cases.columns[0]: feature_name}, inplace=True)
+fig = px.bar(
+    most_cases,
+    y=most_cases.index,
+    x=most_cases[feature_name],
+    text=feature_name,
+    orientation="h",
+    height=700,
+    width=800,
+    # height=800,
+    # width=1000,
+    labels={"location": "Country"},
+    hover_data={feature_name: ":,.0f"},
+)
+fig.update_traces(texttemplate="%{text:.3s}", textposition="outside")
+fig.update_layout(
+    yaxis={"categoryorder": "total ascending"},
+    title_text=f"<b>Countries with the highest {feature_name.lower()}</b><br><i>As of {most_recent.strftime('%B %d, %Y')}</i>",
+)
+st.plotly_chart(fig)
--- utils.py
+++ utils.py
@@ -9,103 +9,122 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-
-def start_pipeline(df):
-    '''Creates a copy of original dataframe to use in pipeline'''
-    return df.copy()
-
-def convert_datetime(df):
-    '''Converts date column to datetime'''
-    df['date'] = df['date'].apply(lambda x: x.to_pydatetime().date())
-    return df
-
-def dropContinents(df):
-  '''Drop those rows whose location are the continents'''
-  drop_idx = df[df['location'].isin(df['continent'].unique()) 
-                | (df['location'] =='World') | (df['location'] =='European Union')].index
-  df= df.drop(drop_idx)
-  return df
-
-def convertCountryLabels(df):
-  '''Correct for differences between geojson country labels and OWID country labels. 
-     Convert all dataframe country labels to what geojson expects.'''
-  
-  df= df.replace({
-            'United States': 'United States of America', 
-            'Serbia': 'Republic of Serbia', 
-            'North Macedonia':'Macedonia', 
-            'Czechia': 'Czech Republic', 
-            "Cote d'Ivoire": 'Ivory Coast', 
-            'Democratic Republic of Congo': 'Democratic Republic of the Congo', 
-            'Congo': 'Republic of the Congo', 
-            'Tanzania': 'United Republic of Tanzania', 
-            'Guinea-Bissau': 'Guinea Bissau', 
-              })
-  return df
-
 @st.cache
 def pivot(df, feature, feature_map):
-    df = df.pivot(index=['location'], 
-                columns='date', 
-                values= feature_map.get(feature, ''))
+    df = df.pivot(
+        index=["location"],
+        columns="date",
+        values=feature_map.get(feature, ""),
+    )
     return df
+
 
 @st.cache
 def daily_increase(df_pivot, most_recent, day_before_most_recent):
     """Calculates the increase of the selected feature between the most recent day and the day before that."""
-    percent_increase = round(((df_pivot[most_recent] - df_pivot[day_before_most_recent]) / df_pivot[most_recent])*100).sort_values(ascending=False)
-    absolute_increase = round(df_pivot[most_recent] - df_pivot[day_before_most_recent]).sort_values(ascending=False)
+    percent_increase = round(
+        (
+            (df_pivot[most_recent] - df_pivot[day_before_most_recent])
+            / df_pivot[most_recent]
+        )
+        * 100
+    ).sort_values(ascending=False)
+    absolute_increase = round(
+        df_pivot[most_recent] - df_pivot[day_before_most_recent]
+    ).sort_values(ascending=False)
 
-    percent_increase = pd.DataFrame(percent_increase[percent_increase != 100], columns=['% increase']).head(20)
-    absolute_increase= pd.DataFrame(absolute_increase, columns=['abs. increase']).head(20)
+    percent_increase = pd.DataFrame(
+        percent_increase[percent_increase != 100], columns=["Percent increase"]
+    ).head(20)
+    absolute_increase = pd.DataFrame(
+        absolute_increase, columns=["Absolute increase"]
+    ).head(20)
     return percent_increase, absolute_increase
 
-    
-#--------- MAPPING
-map_title = { 'new_cases': 'New cases',
-              'new_cases_per_million': 'New cases per million people',
-              'new_cases_smoothed': 'New cases (7 day rolling average)', 
-             'new_cases_smoothed_per_million': 'New cases per million people (7 day rolling average)' 
-              }
-map_colorbar_title = {'new_cases_smoothed': 'New cases', 
-                      'new_cases_smoothed_per_million': 'New cases'}
 
-@st.cache
-def plotly_choropleth(df, date , selected_feature = 'new_cases'):
-  #Get the subset of the dataset matching the user-provided date
-  df = df[df.date == date]
-
-  #Convert date to string format
-  date = date.strftime("%B %d, %Y")
-  fig = go.Figure(data=go.Choropleth(
-    locations = df['iso_code'],
-    z = (df[selected_feature]),
-    text = df['location'], #+ '<br>' + 'New cases ' + str(df_pipeline['new_cases_smoothed']) ,
-    colorscale = [[0, '#ffffff'], [.0005, '#e8cfce'], [.005, '#d09f9c'], [0.1, '#a13e39'],  [0.2, '#8a0e08'], [.25, '#6e0b06'], [1.0, '#450704']],
-    marker_line_color='darkgray',
-    marker_line_width=0.5, 
-    colorbar_title = map_colorbar_title.get(selected_feature, '')
-  ))
-  fig.update_layout(
-      title_text = '<b>' + map_title.get(selected_feature, '') + '</b>' + 
-                   '<br>' +
-                   '<i>' + date + '</i>'  + '<br>' + '(hover over country for the exact number of the selected metric)',
-      width = 120,
-      height=800, 
-      )
-  return fig
+# --------- MAPPING
+MAP_TITLE = {
+    "new_cases": "New cases",
+    "new_cases_per_million": "New cases per million people",
+    "new_cases_smoothed": "New cases (7 day rolling avg)",
+    "new_cases_smoothed_per_million": "New cases per million people (7 day rolling avg)",
+    "total_cases": "Total cases",
+    "total_cases_per_million": "Total cases per million people",
+}
+map_colorbar_title = {
+    "new_cases": "New cases",
+    "new_cases_per_million": "New cases per million people",
+    "new_cases_smoothed": "New cases (7 day rolling avg)",
+    "new_cases_smoothed_per_million": "New cases per million people (7 day rolling avg)",
+    "total_cases": "Total cases",
+    "total_cases_per_million": "Total cases per million people"
+    # "color": "log10(New cases)",
+}
 
 
 @st.cache
-def plotly_bargraph(df, date, date_before_most_recent, selected_feature, option = "% increase"):
-  # fig = go.Figure([go.Bar(x=df.index, y=df[option], hovertext='{:.2%}'.format(list(df[option])))])
-  df = df.reset_index().rename(columns={"index": "Country"})
-  fig = px.bar(df, x="location", y=option, labels={'location': 'Country'})
-  # fig = go.Figure([go.Bar(x=df.index, y=df[option], hovertext=["{:.0%}".format(x/100) for x in df[option]])])
-  fig.update_layout(
-    # font=dict(
-    #     family="Roboto, monospace",
-    #     size=12,
-    # ),
-yaxis_title = f"{option} in {selected_feature.lower()}", title_text = f"<b>Countries with highest daily increase in {selected_feature.lower()}</b> ({option})" + "<br>" + f'<i>From {date_before_most_recent.strftime("%B %d, %Y")} - {date.strftime("%B %d, %Y")}</i>', xaxis_tickangle=-45)
-  return fig
+def plotly_choropleth(df, date, selected_feature="new_cases"):
+    # Get the subset of the dataset matching the user-provided date
+    df = df[df.date == date]
+
+    # Convert date to string format
+    date = date.strftime("%B %d, %Y")
+    fig = px.choropleth(
+        df,
+        locations="iso_code",
+        color=np.log10(df[selected_feature] + 0.01),
+        # color=df[selected_feature],
+        color_continuous_scale="Blues",
+        hover_name="location",
+        hover_data={"iso_code": False, selected_feature: ":,.0f"},
+        labels=map_colorbar_title,
+        # range_color=[df[selected_feature].min(), df[selected_feature].max()],
+    )
+    fig.update_layout(
+        title_text="<b>"
+        + MAP_TITLE.get(selected_feature, "")
+        + "</b>"
+        + "<br>"
+        + "<i>"
+        + date
+        + "</i>"
+        + "<br>",
+        # + "(hover over country for the exact number of the selected metric)",
+        width=120,
+        height=800,
+        coloraxis_colorbar=dict(
+            title=map_colorbar_title.get(selected_feature, ""),
+            tickvals=[-2, 1, 2, 3, 3.699, 4, 5, 6, 7, 7.60],
+            ticktext=[
+                "0",
+                "10",
+                "100",
+                "1,000",
+                "5,000",
+                "10,000",
+                "100,000",
+                "1,000,000",
+                "10,000,000",
+                "40,000,000",
+            ],
+        ),
+    )
+    return fig
+
+
+@st.cache
+def plotly_bargraph(df, most_recent, option):
+    df = df.reset_index().rename(columns={"index": "Country"})
+    fig = px.bar(
+        df,
+        x="location",
+        y=option,
+        hover_name="location",
+        hover_data={"location": False, option: ":,.0f"},
+    )
+    fig.update_layout(
+        yaxis_title=f"{option}",
+        title_text=f"<b>{option} from previous day</b>",
+        xaxis_tickangle=-45,
+    )
+    return fig
